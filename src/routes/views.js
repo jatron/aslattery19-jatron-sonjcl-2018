ae0153b761e8b4a61c6e97569bffaa4187f1975f
Jorge Troncoso <jatron@mit.edu>
2018-01-14 09:47:39 -0500

    Modified views.js to handle /meal_match endpoint

--- src/routes/views.js
+++ src/routes/views.js
@@ -7,4 +7,8 @@ router.get('/', function(req, res, next) {
   res.send('Lets Eat!');
 });
 
+router.get('/meal_match', function(req, res) {
+  res.sendFile('meal_match.html', { root: 'src/views' });
+});
+
 module.exports = router;
