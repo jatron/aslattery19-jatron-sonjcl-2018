4c5f5ec059269aacbc4f49904ac75218d2b52e8b
Jorge Troncoso <jatron@mit.edu>
2018-01-13 20:40:19 -0500

    Implemented basic server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 // main file (we run this javascript program when we start the server)
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Lets Eat!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
