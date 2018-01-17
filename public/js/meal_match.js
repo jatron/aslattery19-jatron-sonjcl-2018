515bc650541714fbefa51378d5ceb49f6fb73650
Alexis Slattery <aslatts@mit.edu>
2018-01-17 16:51:57 -0500

    green border and like icon now appear when photos are selected, info buttton now displays an overlay with recipe information.

--- public/js/meal_match.js
+++ public/js/meal_match.js
@@ -13,4 +13,45 @@ function renderUserData(user) {
 }
 
 
+
+const cards = document.getElementsByClassName("card-img-top");
+for (var i = 0; i < cards.length; i++) {
+    // toggle green border and like icon when clicked
+    cards[i].addEventListener("click", function(){
+        //toggle green border
+        this.classList.toggle('selected');
+        //toggle like icon
+        const checkIcon = document.getElementById(this.id + '-likeIcon');
+        console.log(this.id + '-likeIcon');
+        if (checkIcon.style.opacity == "100") {
+            checkIcon.setAttribute("style", "opacity:0");
+        }
+        else {
+            checkIcon.setAttribute("style", "opacity:100");
+        }
+    });
+}
+
+// create slider with information when info button is clicked
+const infoButtons = document.getElementsByClassName("info-button");
+for (var j = 0; j < infoButtons.length; j++) {
+    infoButtons[j].addEventListener("click", function(){
+        console.log(this);
+        const overlay = document.getElementById(this.id + '-overlay');
+        if (overlay.style.height == "100%") {
+            overlay.setAttribute("style", "height:0%");
+        }
+        else {
+            overlay.setAttribute("style", "height:100%");
+            //overlay.setAttribute("style", "z-index:1");
+        }
+
+        //document.getElementById(this.id + '')
+        
+    });
+}
+
+
+
+
 main();
