0c14ba96698c6e50fb8cb77ce5b24c58b9e7366b
Sonja Lindberg <sonj@mit.edu>
2018-01-25 17:40:02 -0500

    homepage redone, working on info animation but text is there

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,25 @@
-function main(){
-    renderSlideshow();
-}
+// function main(){
+//     renderSlideshow();
+// }
 
-function renderSlideshow(){
-    $(document).ready(function() {
-        $('#slideshow').cycle({
-        fx: 'fade',
-        pager: '#smallnav',
-        pause:   1,
-        speed: 1800,
-        timeout:  3500
-        });
-    });
-}
+// function renderSlideshow(){
+//     $(document).ready(function() {
+//         $('#slideshow').cycle({
+//         fx: 'fade',
+//         pager: '#smallnav',
+//         pause:   1,
+//         speed: 1800,
+//         timeout:  3500
+//         });
+//     });
+// }
 
 
-main();
+// main();
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
