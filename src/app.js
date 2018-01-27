6783c7fba752bbc1e2e58f78f4c11095023e77ac
Jorge Troncoso <jatron@mit.edu>
2018-01-27 17:17:21 -0500

    Moved part of the code in app.js to server.js (Reorganizing code in order to implement instant messaging system)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 // libraries
 require('dotenv').config();
-const http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
@@ -12,9 +11,10 @@ const db = require('./db');
 const passport = require('./passport');
 const views = require('./routes/views');
 const api = require('./routes/api');
+const server = require('./server');
 
 // initialize express app
-const app = express();
+const app = server.app;
 
 // Use middleware that parses urlencoded bodies and json
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,8 +22,8 @@ app.use(bodyParser.json());
 
 
 // // configure socketio
-const server = http.Server(app);
-const io = socketio(server);
+const httpServer = server.httpServer;
+const io = socketio(httpServer);
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -78,6 +78,6 @@ app.use(function(err, req, res, next) {
 
 // port config
 const port = process.env.PORT || 3000; // config variable
-server.listen(port, function() {
+httpServer.listen(port, function() {
     console.log('Server running on port: ' + port);
 });
