0b9ea1ba99804b67f06a0ada6341595b44123de1
Alexis Slattery <aslatts@mit.edu>
2018-01-30 00:23:20 -0500

    profile cards and info overlays now working

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,9 +10,11 @@ function main() {
     // add user string later from profileId
     get('api/profile', {userId : profileId}, function(profileUser) {
     renderUserData(profileUser);
+    renderButtons()
     }, function() {
     console.log("Couldn't access user data :(");
     });
+
 }
 
 
@@ -44,14 +46,20 @@ function renderUserData(user) {
     // $("#edit-profile-btn").href("profile_edit?" + userId);
 
 	// rendering cookbook
-	const cookbookCard = document.getElementById('cookbook-card');
+	const cookbookCard = document.getElementById('meals-container');
 	user.meals.forEach(renderMeals);
 
 	function renderMeals(meal, index, arr) {
+        cards = document.getElementsByClassName("card card-inverse");
+        //console.log(meal);
+
+
+
 		// let li = document.createElement('li');
-        const card = document.createElement('div');
+        //const card = document.createElement('div');
+        const card = cards[index];
         card.setAttribute('id', meal.name);
-        card.className = 'mt-4';
+        //card.className = 'mt-4';
 
         const cardBody = document.createElement('div');
         cardBody.className = 'card-body';
@@ -61,16 +69,71 @@ function renderUserData(user) {
         mealImage.className = 'meal-image-url';
         mealImage.setAttribute('src', meal.url);
         mealImage.className = 'rounded img-fluid';
+        mealImage.setAttribute("id", meal.key);
+        console.log(mealImage.id);
+
         cardBody.appendChild(mealImage);
 
-        // make button box div so that buttons are together
-        const btnBox = document.createElement('div')
-        btnBox.className = 'btn-group'
 
-        renderDropdown(meal.tagline, 'tagline');
-        renderDropdown(meal.description, 'description');
-        renderDropdown(meal.allergens, 'allergens');
-        renderDropdown(meal.ingredients, 'ingredients');
+        // add overlay with information
+        var overlay = document.createElement("div");
+        overlay.setAttribute("id", meal.key + "-info-overlay");
+        overlay.className = 'overlay';
+        card.appendChild(overlay)
+
+        var overlayInfo = document.createElement("div");
+        overlayInfo.setAttribute("class", "information");
+        overlay.appendChild(overlayInfo);
+
+        var description = document.createElement("p");
+        description.innerHTML = meal.description;
+        overlayInfo.appendChild(description);
+
+        var ingredientsTitle = document.createElement("b");
+        ingredientsTitle.innerHTML = "Ingredients:"
+        overlayInfo.appendChild(ingredientsTitle);
+
+        var ingredients = document.createElement("p");
+        ingredients.innerHTML = meal.ingredients;
+        overlayInfo.appendChild(ingredients);
+
+        var allergensTitle = document.createElement("b");
+        allergensTitle.innerHTML = "Allergens:";
+        overlayInfo.appendChild(allergensTitle);
+
+        var allergens = document.createElement("p");
+        allergens.innerHTML = meal.allergens;
+        overlayInfo.appendChild(allergens);
+
+
+        var buttonGroupDiv = document.createElement("div");
+        buttonGroupDiv.setAttribute("class", "btn-group");
+        buttonGroupDiv.setAttribute("role", "group");
+        card.appendChild(buttonGroupDiv);
+
+        //add info button
+        var infoButton = document.createElement('button');
+        infoButton.setAttribute('type', "button");
+        infoButton.setAttribute("id", meal.key + '-info');
+        infoButton.setAttribute('class', "btn info-button");
+        buttonGroupDiv.appendChild(infoButton);
+        // add info icon
+        var infoIcon = document.createElement('i');
+        infoIcon.setAttribute('class', 'fa fa-info-circle');
+        infoIcon.setAttribute('id', 'fa-info-circle');
+        infoIcon.setAttribute('aria-hidden', 'true');
+        infoIcon.setAttribute('style', "font-size:30px");
+        infoButton.appendChild(infoIcon);
+
+
+        // make button box div so that buttons are together
+        // const btnBox = document.createElement('div')
+        // btnBox.className = 'btn-group'
+
+        // renderDropdown(meal.tagline, 'tagline');
+        // renderDropdown(meal.description, 'description');
+        // renderDropdown(meal.allergens, 'allergens');
+        // renderDropdown(meal.ingredients, 'ingredients');
 
 
         // render dropdown button
@@ -119,4 +182,25 @@ function renderUserData(user) {
 
 }
 
+function renderButtons() {
+
+// create slider with information when info button is clicked
+const infoButtons = document.getElementsByClassName("info-button");
+for (var j = 0; j < infoButtons.length; j++) {
+    console.log(infoButtons[j]);
+    infoButtons[j].addEventListener("click", function(){
+        const overlay = document.getElementById(this.id + '-overlay');
+        if (overlay.style.height == "100%") {
+            overlay.setAttribute("style", "height:0%");
+        }
+        else {
+            overlay.setAttribute("style", "height:100%");
+            //overlay.setAttribute("style", "z-index:1");
+        }
+
+        //document.getElementById(this.id + '')
+        
+    });
+}
+}
 main();
